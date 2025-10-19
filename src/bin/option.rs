82c8e3e425620ecc88d692e848ccbf3987992f1a
puggo <puggoo1145@gmail.com>
2025-10-18 17:26:22 -0700

    feat: 🎸 learned option and better control flow

--- src/bin/option.rs
+++ src/bin/option.rs
@@ -5,8 +5,27 @@ fn add(a: u32, b: Option<u32>) -> Result<u32, bool> {
     }
 }
 
+fn check_val(val: Option<u32>) -> Option<u32> {
+    let Some(state) = val else {
+        println!("Value not exists");
+        return None;
+    };
+
+    Some(state)
+}
+
 fn main() {
     let b: Option<u32> = Some(5);
     let res = add(4, b);
     println!("{:?}", res);
+
+    let some_val = Some(1);
+    if let Some(val) = some_val {
+        println!("{val}");
+    } else {
+        println!("nothing")
+    }
+
+    let res = check_val(Option::None);
+    println!("{res:?}")
 }
