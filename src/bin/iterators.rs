26723ccee2170521c0194addf85305f572ad9e4f
puggo <puggoo1145@gmail.com>
2025-11-02 17:58:19 -0800

    feat: 🎸 iter

--- src/bin/iterators.rs
+++ src/bin/iterators.rs
@@ -1,8 +1,42 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
 fn main() {
-    let v1 = vec![1, 2, 3];
-    let v1_iter = v1.iter();
-    let v1_map_iter = v1_iter.map(|x| x + 1);
-    for val in v1_map_iter {
-        println!("Mapped to {val}");
-    }
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+
+    let in_my_size = shoes_in_size(shoes, 10);
+
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot")
+            },
+        ]
+    );
 }
